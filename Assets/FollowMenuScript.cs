d894fe8793fff5e5947050252ed2f978512bd049
Mohammad Rajabi <rajabiseraji@outlook.com>
2021-09-27 16:24:12 -0700

    feat(floatingDataShelf): adds the button to start dev

--- Assets/FollowMenuScript.cs
+++ Assets/FollowMenuScript.cs
@@ -1,17 +1,79 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using DG.Tweening;
 
-public class FollowMenuScript : MonoBehaviour
+public class FollowMenuScript : MonoBehaviour, Grabbable
 {
     public GameObject Camera;
     public bool FollowCamera = true;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+
+    [SerializeField]
+    UnityEvent OnEntered;
+
+    [SerializeField]
+    UnityEvent OnExited;
+
+    public Vector3 initialScale;
+    Vector3 rescaled = Vector3.one;
+
+    // Use this for initialization
+    void Start () {
+        //initialScale = transform.localScale;
+        rescaled = initialScale;
+        rescaled.x *= 2f;
+        rescaled.y *= 2f;
+        rescaled.z *= 2f;
     }
 
+    public int GetPriority()
+    {
+        return 100; // I should work with this to have the highest priority
+    }
+
+    public void OnDrag(WandController controller)
+    {
+        // I don't think we should do anything about the dragging of this thing
+    }
+
+    public void OnEnter(WandController controller)
+    {
+        OnEntered.Invoke();
+    }
+
+    public void OnExit(WandController controller)
+    {
+        OnExited.Invoke();
+    }
+
+    public bool OnGrab(WandController controller)
+    {
+        // We should simply act as the ontouch of a button and show the childs of it which are the data shelf items
+        // it needs to be a toggle situation - clicked: on/off
+
+        return false; // it's not supposed to be draggable, so return false for this
+    }
+
+    public void OnRelease(WandController controller)
+    {
+        // 
+    }
+
+     public void ProximityEnter()
+    {
+        transform.DOKill(true);
+        // transform.DOLocalMoveX(-axisOffset, 0.35f).SetEase(Ease.OutBack);
+        transform.DOScale(rescaled, 0.35f).SetEase(Ease.OutBack);
+    }
+
+    public void ProximityExit()
+    {
+        transform.DOKill(true);
+        // transform.DOLocalMoveX(0, 0.25f);
+        transform.DOScale(initialScale, 0.25f);
+    }
+    
     // Update is called once per frame
     void Update()
     {
