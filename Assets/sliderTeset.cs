9c18457e759441e36dd1caa88026c7334876a535
Mohammad Rajabi <rajabiseraji@outlook.com>
2022-04-27 16:45:24 -0700

    fix(canvasLaser): fixes the canvas laser input handler
    now we can use the filters

---
+++ Assets/sliderTeset.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem.UI;
+using UnityEngine.EventSystems;
+
+public class sliderTeset : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public static void OnDragEnter()
+    {
+        print("there's a drag happening");
+    }
+}
